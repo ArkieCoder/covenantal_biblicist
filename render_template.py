d25a3c3c9435bb2954c7d6ada2aa146e16fc5017
jhr <jhr@jhrs-MacBook-Neo.local>
2026-07-17 12:33:22 -0500

    commit generated stuff to repo / parameterize api key

--- render_template.py
+++ render_template.py
@@ -2,6 +2,7 @@
 """Render a Jinja2 article template with the given article name."""
 
 import sys
+import os
 import yaml
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
@@ -19,10 +20,13 @@ if metadata_path.exists():
     article_title = meta.get("title", article_title)
     article_description = meta.get("description", "")
 
+adobe_client_id = os.environ.get("ADOBE_CLIENT_ID", "bd4cb7ccbcdc4940adb98b75683d410a")
+
 env = Environment(loader=FileSystemLoader(template_dir))
 template = env.get_template(template_name)
 print(template.render(
     article_name=article_name,
     article_title=article_title,
-    article_description=article_description
+    article_description=article_description,
+    adobe_client_id=adobe_client_id
 ), end="")
